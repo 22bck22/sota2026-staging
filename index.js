814696b56c770f078fbbd771cd5b13bc779afec3
Barrett Kim <22bck22@gmail.com>
2026-05-17 12:39:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -652,7 +652,7 @@ window.onload = () => {
     inputIsPercentage: false,
     sorted: false,
     showLegend: false,
-    title: "Affiliation & Change",
+    title: "Change in Political Beliefs in the Past Year",
     subtitle:
       "Whether respondents did or did not change beliefs by their political affiliations.",
     // 2025
